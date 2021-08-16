0f9bedea09ad0d1736dac5d0e8aa7d6403160dc0
princeton98 <princeton.arul@gmail.com>
2021-08-15 21:42:11 -0400

    first commit

---
+++ portfolio/src/components/Navbar/NavbarPortfolio.js
@@ -0,0 +1,27 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import "./NavbarPortfolio.css"
+
+
+const NavbarPortfolio = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Projects</Nav.Link>
+        <Nav.Link href="#link">Resume</Nav.Link>
+        <Nav.Link href="#link">Sandbox</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
+
+export default NavbarPortfolio;
